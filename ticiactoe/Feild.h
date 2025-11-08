3067d1189a255f7253d3f8b8a33d012788746e4f
cpp <roma.minkov1@gmail.com>
2025-11-08 13:56:43 +0100

    first commit

---
+++ ticiactoe/Feild.h
@@ -0,0 +1,130 @@
+//#pragma once
+#ifndef one
+#define one
+
+
+void printFeild(char field[][3])
+{
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            std::cout << ' ' << field[i][j] << ' ';
+            if (j < size - 1) std::cout << '|';
+        }
+        std::cout << '\n';
+        if (i < size - 1) std::cout << "---+---+---\n";
+    }
+    std::cout << "\n";
+}
+int Pchoose()
+{
+    int a;
+    std::cin >> a;
+    return a - 1;
+}
+bool fillbox(char s, int p, char* field[])
+{
+    if (Fsize > p and p >= 0) {
+        if (*field[p] == ' ') { *field[p] = s; return 1; }
+    }
+    return 0;
+}
+
+void fillfield(char symbol, char* field[])
+{
+    while (true)
+    {
+        int place = Pchoose();
+        if (fillbox(symbol,place, field)){break;}
+    }
+}
+
+
+bool iswin(char* b[], char l)
+{
+    if ((*b[7] == l && *b[4] == l && *b[1] == l) ||
+        (*b[0] == l && *b[4] == l && *b[8] == l) ||
+        (*b[0] == l && *b[3] == l && *b[6] == l) ||
+        (*b[0] == l && *b[1] == l && *b[2] == l) ||
+        (*b[3] == l && *b[4] == l && *b[5] == l) ||
+        (*b[6] == l && *b[7] == l && *b[8] == l) ||
+        (*b[2] == l && *b[4] == l && *b[6] == l) ||
+        (*b[2] == l && *b[5] == l && *b[8] == l)) {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+bool turn(char symbol, char* field[], char mode, char isP, char dif, char trueP)
+{
+    if (mode == 'p')
+    {
+        fillfield(symbol, field);
+    }
+    else
+    {
+        if (isP == 'X')
+        {
+            fillfield(symbol, field);
+        }
+        else
+        {
+            // hard складнисть блокує угрока якщо 2 вже соять
+            if (dif == 'h')
+            {
+                for (int i = 0; i < Fsize; ++i)
+                {
+                    if (fillbox(trueP, i, field))
+                    {
+                        if (iswin(field, trueP) == 0)
+                        {
+                            *field[i] = ' ';
+                            fillbox(symbol, i, field);
+                            return 0;
+                        }
+                        *field[i] = ' ';
+                    }
+                }
+            }
+            // нормальна складнисть збирає ряд якщо 2 вже соять
+            if (dif == 'n' or dif == 'h')
+            {
+                for (int i = 0; i < Fsize; ++i)
+                {
+                    if (fillbox(symbol, i, field))
+                    {
+                        if (iswin(field, symbol) == 0)
+                        {
+                            return 0;
+                        }
+                        *field[i] = ' ';
+                    }
+                }
+                // ход по кутам
+                for(int i = rand() % 5; i <4;i += 1 )
+                    if (fillbox(symbol, corners[ i], field)) { return 0; }
+            }
+            //рандом ход
+            while (true) {
+                int place = rand() % Fsize;
+                if (fillbox(symbol, place, field)) { return 0; }
+            }
+        }
+    }
+}
+void endgame(int isP, char ox[], char symbol, char mode)
+{
+    if (ox[isP] == symbol)
+    {
+        std::cout << "You win!";
+        if (mode != 'p') { std::cout << "AI is defeated"; }
+    }
+    else
+    {
+        std::cout << "You lose!";
+        if (mode != 'p') { std::cout << "AI destroy the world"; }
+    }
+}
+#endif
---
+++ ticiactoe/ticiactoe.cpp
@@ -0,0 +1,67 @@
+﻿// ticiactoe.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Feild.h"
+const int size = 3;
+const int Fsize = 9;
+int corners[4] = { 0,2,6,8 };
+char ox[2]{ 'O','X' };
+
+
+int main()
+{
+
+    char field[size][size]{};
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            field[i][j] = ' ';
+        }
+    };
+
+    char* ifield[Fsize]{ &field[0][0] ,&field[0][1] ,&field[0][2] , &field[1][0] , &field[1][1] ,&field[1][2] ,&field[2][0] ,&field[2][1] ,&field[2][2] };
+    std::cout << "------------------------GAME KRESTIKI NOLIKI-------------------------------\n";
+    printFeild(field);
+    std::cout << "To play print nmbers from 1 to 9:\n";
+
+    char mode, dif = 'o';
+    char buffer[50];
+    while (true)
+    {
+        std::cout << "Play aganst Bot or aganst Player?(b or p)";
+        std::cin >> buffer;
+        mode = buffer[0];
+        if (mode == 'b' or mode == 'p') { break; }
+    }
+    if (mode == 'b') {
+           while (true)
+           {
+               std::cout << "Choose difikulty easy (e), normal (n),hard (h)";
+               std::cin >> buffer;
+               dif = buffer[0];
+               if (dif == 'e' or dif == 'n' or dif == 'h') { break; }
+           }
+       }
+    srand(time(NULL));
+    int isP = rand() % 2;
+
+
+    std::cout << "you play " << ox[isP] << "\n";
+    
+    int turns = 1;
+    while (turns != 9)
+    {
+        turn('X', ifield, mode, ox[isP], dif, ox[isP]);
+        printFeild(field);
+        if (iswin(ifield, 'X') == 0) { endgame(isP, ox, 'X', mode); break; }
+        ++turns;
+        turn('O', ifield, mode, ox[!isP], dif, ox[isP]);
+        printFeild(field);
+        if (iswin(ifield, 'O') == 0) { endgame(isP, ox, 'O', mode); break; }
+        ++turns;
+    }
+    if (turns == 9)
+    {
+        std::cout << "Friendship\n";
+    }
+}
